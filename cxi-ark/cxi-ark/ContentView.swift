1d33563ea1c5c5ed805ccf44eb1c6ceeafe9053e
Austin <agladfelter@mail.bradley.edu>
2019-11-25 11:33:52 -0600

    Initial commit - all project files.

---
+++ cxi-ark/cxi-ark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cxi-ark
+//
+//  Created by Austin Gladfelter on 11/25/19.
+//  Copyright © 2019 Austin Gladfelter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
